7618b18a45239b891e8b22bcd25e89280d4ae286
mknowles <mknowles@mit.edu>
2016-12-12 12:36:24 -0500

    Emergency save of lcm-to-rosbag

--- scripts/lcm-to-rosbag.py
+++ scripts/lcm-to-rosbag.py
@@ -48,7 +48,7 @@ class BagfileMaker(object):
   	elif imu_msg.header.stamp.secs > self.maxTime:
   		self.maxTime = imu_msg.header.stamp.secs
 
-  	imu_msg.header.frame_id = 'imu'
+  	imu_msg.header.frame_id = 'imu4'
   	imu_msg.orientation.w = lcm_msg.quat[0]
   	imu_msg.orientation.x = lcm_msg.quat[1]
   	imu_msg.orientation.y = lcm_msg.quat[2]
@@ -81,7 +81,7 @@ class BagfileMaker(object):
   	#make a new rosbag image msg
   	img_msg.header.seq = self.seq
   	img_msg.header.stamp.secs, img_msg.header.stamp.nsecs = getSecsNSecs(lcm_msg.utime)
-  	img_msg.header.frame_id = 'cam'
+  	img_msg.header.frame_id = 'cam0'
 
   	# increment the seq for the next message
   	self.seq += 1
@@ -90,29 +90,30 @@ class BagfileMaker(object):
   	self.bag.write(cam_topic_name, img_msg, img_msg.header.stamp)
 
   def viconHandler(self, event, vicon_topic_name):
-  	"""
-  	Convert LCM vicon message into a ROS geometry_msgs/TransformStamped msg
-  	"""
-  	lcm_msg = rigid_transform_t.decode(event.data)
-  	vicon_msg = TransformStamped()
+    """
+    Convert LCM vicon message into a ROS geometry_msgs/TransformStamped msg
+    """
+    lcm_msg = rigid_transform_t.decode(event.data)
+    vicon_msg = TransformStamped()
 
-  	#build the header
-  	vicon_msg.header.seq = self.seq
-  	vicon_msg.header.stamp.secs, vicon_msg.header.stamp.nsecs = getSecsNSecs(lcm_msg.utime)
-  	vicon_msg.header.frame_id = 'vicon'
+    #build the header
+    vicon_msg.header.seq = self.seq
+    vicon_msg.header.stamp.secs, vicon_msg.header.stamp.nsecs = getSecsNSecs(lcm_msg.utime)
+    vicon_msg.header.frame_id = 'vicon_world'
+    vicon_msg.child_frame_id = 'truth'
 
-  	vicon_msg.transform.translation.x = lcm_msg.trans[0]
-  	vicon_msg.transform.translation.y = lcm_msg.trans[1]
-  	vicon_msg.transform.translation.z = lcm_msg.trans[2]
+    vicon_msg.transform.translation.x = lcm_msg.trans[0]
+    vicon_msg.transform.translation.y = lcm_msg.trans[1]
+    vicon_msg.transform.translation.z = lcm_msg.trans[2]
 
-  	vicon_msg.transform.rotation.w = lcm_msg.quat[0]
-  	vicon_msg.transform.rotation.x = lcm_msg.quat[1]
-  	vicon_msg.transform.rotation.y = lcm_msg.quat[2]
-  	vicon_msg.transform.rotation.z = lcm_msg.quat[3]
+    vicon_msg.transform.rotation.w = lcm_msg.quat[0]
+    vicon_msg.transform.rotation.x = lcm_msg.quat[1]
+    vicon_msg.transform.rotation.y = lcm_msg.quat[2]
+    vicon_msg.transform.rotation.z = lcm_msg.quat[3]
 
-  	# increment the seq for the next message
-  	self.seq += 1
-  	self.bag.write(vicon_topic_name, vicon_msg, vicon_msg.header.stamp)
+    # increment the seq for the next message
+    self.seq += 1
+    self.bag.write(vicon_topic_name, vicon_msg, vicon_msg.header.stamp)
 
   def closeBag(self):
   	print("Finished writing", self.seq, "messages to rosbag file.")
@@ -140,8 +141,8 @@ def main():
 	bagfile_dir = '/home/mknowles/bagfiles/star/'
 
 	#NOTE: change the names here!!!!!
-	lcm_file_name = 'lcmlog-2015-08-18.02' #18.00 through 18.18
-	bagfile_name = 'star2.bag'
+	lcm_file_name = 'lcmlog-2015-08-18.01' #18.00 through 18.18
+	bagfile_name = 'star1.bag'
 
 	#define rosbag output topic names
 	cam_topic_name = '/cam0/image_raw'
